a838db4a9629ed577c60cafb66ee492f8baaa22d
Anandakrishnan H <sree403020@gmail.com>
2024-03-15 23:14:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface Student {
+  name: string;
+  age: number;
+  department: string;
+  totalMarks: number;
+}
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
+}) 
+
 export class AppComponent {
   title = 'array-sample';
+  
+  students: Student[] = [
+    {name: 'Anu', age: 24, department: 'MCA', totalMarks: 700},
+    {name: 'Manoj', age: 21, department: 'BCA', totalMarks: 650},
+    {name: 'Sujith', age: 25, department: 'MBA', totalMarks: 710},
+    {name: 'Rahul', age: 24, department: 'MCA', totalMarks: 750},
+    {name: 'Krishna', age: 21, department: 'BCA', totalMarks: 610},
+    {name: 'Surya', age: 25, department: 'MBA', totalMarks: 725},
+    {name: 'Sanker', age: 24, department: 'MCA', totalMarks: 680},
+    {name: 'Sandra', age: 21, department: 'BCA', totalMarks: 627},
+    {name: 'Athira', age: 25, department: 'MBA', totalMarks: 740},
+    {name: 'Joyal', age: 24, department: 'MCA', totalMarks: 728},
+    {name: 'Sanuja', age: 21, department: 'BCA', totalMarks: 687},
+    {name: 'Kiran', age: 25, department: 'MBA', totalMarks: 790},
+    {name: 'Nishad', age: 21, department: 'BCA', totalMarks: 600},
+    {name: 'Praveena', age: 25, department: 'MBA', totalMarks: 800}
+  ];
+
+  filterMark : number = 0;
+  filteredStudents: Student[] = [];
+
+  filterStudents() {
+    if (this.filterMark !== null && this.filterMark > 0) {
+      this.filteredStudents = this.students.filter(student => student.totalMarks > this.filterMark);
+    } else {
+      this.filteredStudents = this.students;
+    }
+  }
+
+  ngOnInit() {
+    this.filterStudents();
+  }
 }
+
+
+
+
